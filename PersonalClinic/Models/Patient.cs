3ea7dda5f3cc280118e638aae92af46bd2de3fee
Aditya Wibisana <aditya.wibisana@gmail.com>
2017-03-23 20:08:57 +0700

    Add models : Patient and History

---
+++ PersonalClinic/Models/Patient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonalClinic.Models
+{
+    public class Patient
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+        public String PhoneNumber { get; set; }
+        public String Address { get; set; }
+        public String Identifier { get; set; }
+        public List<History> Histories { get; set; } 
+    }
+}
