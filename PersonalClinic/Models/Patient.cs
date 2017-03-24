946413b7901bd6fe493f2c9e7f4981bd4b556927
Aditya Wibisana <aditya.wibisana@gmail.com>
2017-03-24 13:53:42 +0700

    Change display name

--- PersonalClinic/Models/Patient.cs
+++ PersonalClinic/Models/Patient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PersonalClinic.Models
@@ -9,10 +10,22 @@ namespace PersonalClinic.Models
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
 
+        [Display(Name="Nama")]
         public String Name { get; set; }
+
+        [Display(Name = "Nomer Handphone")]
         public String PhoneNumber { get; set; }
+
+        [Display(Name="Alamat")]
         public String Address { get; set; }
+
+        [Display(Name = "KTP / ID Pengenal")]
         public String Identifier { get; set; }
+
+        [Display(Name="Tanggal Lahir")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        
         public List<History> Histories { get; set; } 
     }
 }
