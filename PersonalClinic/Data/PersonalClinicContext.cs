43e15155102a139fa0c980cf9853a78ca379f009
Aditya Wibisana <aditya.wibisana@gmail.com>
2017-03-24 13:39:47 +0700

    Initial creation of Patients Model

---
+++ PersonalClinic/Data/PersonalClinicContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersonalClinic.Models
+{
+    public class PersonalClinicContext : DbContext
+    {
+        public PersonalClinicContext (DbContextOptions<PersonalClinicContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PersonalClinic.Models.Patient> Patient { get; set; }
+    }
+}
